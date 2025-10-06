7d86856a211dd2712f973aa821daff8baba1637c
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-06 20:17:33 +0200

    trabajando con @if y @for

--- MiPrimerProyecto/src/app/components/semaforo/semaforo.ts
+++ MiPrimerProyecto/src/app/components/semaforo/semaforo.ts
@@ -7,18 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './semaforo.css'
 })
 export class Semaforo {
-  numero: number = 0;
+  numero: number = 10;
+  frutas: string[] = ["Pera", "Banana", "Manzana", "Uva"];
+  cuadrados: number[] = [1, 1, 0, 1, 0, 0, 1, 0, 0];
+
   incrementar() {
     if (this.numero < 10) {
       this.numero++;
+    } else {
+      this.numero = 10;
     }
   }
   decrementar() {
     if (this.numero > 0) {
       this.numero--;
+    } else {
+      this.numero = 0;
     }
   }
   reset() {
-    this.numero = 0;
+    this.numero = 10;
   }
 }
