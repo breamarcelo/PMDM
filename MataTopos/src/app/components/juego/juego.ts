3d6c58d20d0dd909f1931a4f1ca2d75448a4aee8
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-08 16:19:16 +0200

    pendiente corregir while

--- MataTopos/src/app/components/juego/juego.ts
+++ MataTopos/src/app/components/juego/juego.ts
@@ -10,6 +10,19 @@ export class Juego {
   casillas: number[] = [0, 0, 0, 0, 1, 0, 0, 0, 0];
   puntuacion: number = 0;
 
+  generarTablero() {
+    let num: number = this.casillas.indexOf(1);
+    while ()
+      num = Math.floor(Math.random() * 8);
+    for (let i = 0; i < 9; i++) {
+      if (i != num) {
+        this.casillas[i] = 0;
+      } else if (i == num) {
+        this.casillas[i] = 1;
+      }
+    }
+  }
+
   actualizarPuntuacion(casilla: number) {
     if (casilla == 1) {
       this.puntuacion++;
