65e3f85b45e630b26c479955b95e0a596eb3326d
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-30 13:34:44 +0100

    iniciado Halloween

---
+++ Halloween/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Formulario } from './components/formulario/formulario';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Formulario],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Halloween');
+}
---
+++ Halloween/src/app/components/formulario/formulario.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-formulario',
+  imports: [ReactiveFormsModule, MatInputModule, MatSelectModule, MatCheckboxModule, MatButtonModule, MatIconModule],
+  templateUrl: './formulario.html',
+  styleUrl: './formulario.css',
+
+})
+export class Formulario {
+  formulario: FormGroup;
+  tipoInvitado: string = "";
+
+  constructor(private fb: FormBuilder) {
+    this.formulario = fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      disfraz: ['', Validators.required],
+      fechaLlegada: ['', Validators.required],
+      aceptarReglas: ['', Validators.required],
+    });
+  }
+
+  enviar() {
+
+  }
+}
