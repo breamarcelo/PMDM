a71d520608672b33902b878a6e5e33ff56fe99c6
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-11-12 22:41:07 +0100

    ngclass

--- MiPrimerProyecto/src/app/components/nav-bar/nav-bar.ts
+++ MiPrimerProyecto/src/app/components/nav-bar/nav-bar.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, MatMenuModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, MatMenuModule, MatSlideToggleModule, FormsModule, CommonModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
 })
-export class NavBar {
+export class NavBar implements OnInit, OnDestroy {
+  isChecked = true;
+  intervalo !: any;
 
+  cambiarFondo() {
+    return this.isChecked ? { 'background-color': 'lightblue' } : { 'background-color': 'transparent' };
+  }
+
+  cambiarClase() {
+    return this.isChecked ? "fondo-azul" : "fondo-transparente";
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalo);
+  }
+
+  ngOnInit(): void {
+    this.cambiarIsChecked();
+
+    console.log("entramos en ngOnInit");
+  }
+
+  cambiarIsChecked() {
+    this.intervalo = setInterval(() => { this.isChecked = !this.isChecked; console.log(this.isChecked) }, 1000);
+  };
 }
