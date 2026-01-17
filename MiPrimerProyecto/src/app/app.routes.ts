80d0fc45b55d3faf77fe267c9ebc5a93498e4704
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-01-17 11:03:12 +0100

    prubeas Routes

--- MiPrimerProyecto/src/app/app.routes.ts
+++ MiPrimerProyecto/src/app/app.routes.ts
@@ -7,5 +7,5 @@ import { ListaPersonajes } from './components/lista-personajes/lista-personajes'
 export const routes: Routes = [
     { path: 'semaforo', component: Semaforo },
     { path: 'formulario', component: Formulario },
-    { path: 'lista-personajes', component: ListaPersonajes }
+    { path: 'lista-personajes', component: ListaPersonajes },
 ];
